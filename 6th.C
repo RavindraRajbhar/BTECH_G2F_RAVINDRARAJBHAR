19f3e4134f1263c420a4476e7015acd82ee66f3c
RavindraRajbhar <ravindraxrajbhar@gmail.com>
2026-02-17 12:07:50 +0530

    Create 6th.C
    WAP that checks whether the two numbers entered by the user are equal or not

---
+++ 6th.C
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two no:");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a==b)
+    printf("The numbers are equal.");
+    else 
+    printf("The numbers are not equal.");
+    return 0;
+
+}
