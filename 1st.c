8266192a60d9c4b1f3d350e73feaa7eda2e0f6b0
RavindraRajbhar <ravindraxrajbhar@gmail.com>
2026-02-17 11:06:09 +0530

    Create 1st.c
    WAP that accept the mark of 5 subject and find the sum and percentage marks obtained by the student.

---
+++ 1st.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int m1, m2, m3, m4, m5;
+    
+    printf("\n Enter the marks: ");
+    
+    scanf("%d%d%d%d%d", &m1, &m2, &m3, &m4, &m5);
+    
+    int sum = m1 + m2 + m3 + m4 + m5;
+    float per = sum / 5.0;
+    
+    printf("\n Total marks: %d", sum);
+    
+    printf("\n Percentage = %2f", per); 
+
+    return 0;
+}
